fb0994653d6888f031461d671b3cb041acc61eaa
Sean0z <seanozinsky@gmail.com>
2026-07-05 18:11:07 -0700

    Github pages hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sean0z.github.io',
+   base: '/my-portfolio/',
   vite: {
     plugins: [tailwindcss()],
   },
